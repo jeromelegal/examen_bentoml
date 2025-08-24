062f80b3ced6d1c30e05f4fa047533bd9f76cc47
garthcrow <garth@phylcero.fr>
2025-08-24 13:29:13 +0000

    Add train_model.py

---
+++ src/models/train_model.py
@@ -0,0 +1,42 @@
+### Import librairies
+import pandas as pd
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import bentoml
+
+### Path
+processed_path = os.path.join("..", "..", "data", "processed/")
+
+### Import datasets
+X_train = pd.read_csv(processed_path + 'X_train.csv')
+X_test = pd.read_csv(processed_path + 'X_test.csv')
+y_train = pd.read_csv(processed_path + 'y_train.csv')
+y_test = pd.read_csv(processed_path + 'y_test.csv')
+
+### Model
+lr = LinearRegression()
+
+### Train model
+lr.fit(X_train, y_train)
+
+### Evaluate
+y_pred_train = lr.predict(X_train)
+y_pred = lr.predict(X_test)
+r2_train = r2_score(y_pred_train, y_train)
+r2 = r2_score(y_pred, y_test)
+
+print(f"Le score R² train est : {r2_train}")
+print(f"Le score R² test est : {r2}")
+
+### Save model in Bentoml
+model_ref = bentoml.sklearn.save_model("admission_lr", lr)
+print(f"Modèle enregistré sous : {model_ref.tag}")
+
+### Verify saving
+bentoml_model_list = []
+for m in bentoml.models.list():
+    bentoml_model_list.append(m.tag)
+
+if model_ref.tag in bentoml_model_list:
+    print(f"Modèle 'admission:latest' bien trouvé comme : {m.tag}")
